579805ef158abb9a39663ce1f4a8ae259bc3e302
unknown <darren06nb@gmail.com>
2023-09-14 21:13:20 +0700

    CS02 - Menyelesaikan Pre CS

---
+++ darrenNathanaelBoentaraJBusIO/Rating.java
@@ -0,0 +1,14 @@
+package darrenNathanaelBoentaraJBusIO;
+
+public class Rating
+{
+    // instance variables - replace the example below with your own
+    private long count;
+    private long total;
+
+    public Rating()
+    {
+        this.total = 0;
+        this.count = 0;
+    }
+}
