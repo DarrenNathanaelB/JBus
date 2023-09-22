0ce2c20f789310c4ec980a954906c2e1cea40574
unknown <darren06nb@gmail.com>
2023-09-22 11:42:00 +0700

    CS03 - Menyelesaikan CS03

--- darrenNathanaelBoentaraJBusIO/JBus.java
+++ darrenNathanaelBoentaraJBusIO/JBus.java
@@ -8,14 +8,21 @@ public class JBus
         System.out.println(testBus.name);
         System.out.println(testBus.facility);
         System.out.println(testBus.price.price);
-        System.out.println(testBus.capacity);**/
+        System.out.println(testBus.capacity);
         
         Price tesPrice = new Price(200);
         Voucher tes = new Voucher(1,"halo", 200, Type.REBATE, 150, 10);
 
         System.out.println(tes.name);
         System.out.println(tes.canApply(tesPrice));
-        System.out.println(tes.apply(tesPrice));
+        System.out.println(tes.apply(tesPrice));**/
+        
+        Payment testPayment = new Payment(1, 1, 1, "A", 1, "A", "A");
+        Invoice testInvoice = new Invoice(2,2,2, "B");
+        Station testStation = new Station(3, "C", City.DEPOK);
+        System.out.println(testPayment.print());
+        System.out.println(testInvoice.print());
+        System.out.println(testStation.print());
     }
 
     /**public static Bus createBus(){
---
+++ darrenNathanaelBoentaraJBusIO/Payment.java
@@ -0,0 +1,34 @@
+package darrenNathanaelBoentaraJBusIO;
+
+public class Payment extends Invoice
+{
+    private int busId;
+    public String departureDate;
+    public String busSeat;
+
+    public Payment(int id, int buyerId, int renterId, String time, int busId, String departureDate, String busSeat)
+    {
+        super(id, buyerId, renterId, time);
+        this.busId = busId;
+        this.departureDate = departureDate;
+        this.busSeat = busSeat;
+    }
+
+    public Payment(int id, Account buyer, Renter renter, String time, int busId, String departureDate, String busSeat)
+    {
+        super(id, buyer, renter, time);
+        this.busId = busId;
+        this.departureDate = departureDate;
+        this.busSeat = busSeat;
+    }
+    
+    public String print()
+    {
+        return "Id: " + id +" Buyer Id: " + buyerId + " Renter Id: " + renterId + " time: "+ time + " Bus Id: " + busId + " date: " + departureDate + " seat: " + busSeat;
+    }
+    
+    public int getBusId()
+    {
+        return busId;
+    }
+}
